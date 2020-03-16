645af94450bb30ba25eeb498a58272d9b2be163e
Evgeny Lobachev <evgeny@sigmaltd.ru>
2020-03-16 17:40:27 +0300

    add Step_2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,17 @@ const program = require('commander');
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action(function(firstConfig, secondConfig) {
+    if (firstConfig) {
+      console.log(firstConfig);
+    }
+    if (secondConfig) {
+      console.log(secondConfig);
+    }
+  });
 
 program.parse(process.argv);
 
